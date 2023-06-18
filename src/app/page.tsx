8050ad1382a8991b4ba018b7093ba36d8c037311
Mukul108 <Mukulpal108@hotmail.com>
2023-06-19 01:15:50 +0530

    lot of cng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
 
       <div className="relative flex flex-col items-center justify-between p-24">
         <header className="absolute top-0 left-0 right-0 flex items-center justify-between p-6 z-10">
-          <div className={`bg-black bg-opacity-50 rounded-lg p-4 flex items-center justify-between w-full ${styles.btnContainer}`}>
-            <h1 className="text-4xl font-bold text-white">Welcome to Rai Gymnastics</h1>
+          <div className={`bg-white bg-opacity-20 rounded-lg p-4 flex items-center justify-between w-full ${styles.btnContainer}`}>
+            <h1 className="text-4xl font-bold text-black">Welcome to Rai Gymnastics</h1>
             <nav className="flex gap-4">
               <a href="#" className={`btn ${styles.navBtn}`}>About</a>
               <a href="#" className={`btn ${styles.navBtn}`}>Achievements</a>
@@ -29,23 +29,36 @@ export default function Home() {
         </header>
 
         <div className="flex justify-center mt-8">
-          <div className={`bg-white bg-opacity-20 rounded-lg p-10 flex items-center justify-center ${styles.contentSquare} mr-4`}>
-            <div>
-              <h2 className="text-4xl text-white mb-4">We are the Champions</h2>
-              <p className="text-2xl text-white ">We have broken the record by winning 48 medals in the Senior Nationals 2023-24.</p>
-              <p className="text-2xl text-white ">All the credits goes to our coach Dr. Takdir kundu</p>
+        
+         <div className={`box ${styles.contentSquare} ml-4`}>
+          <div>
+          
+          
+         
+              <h2 className="welcome-text">We are the Champions</h2>
+              
+              <p className="description">
+               We have achieved tremendous success in gymnastics, winning 48 medals in the Senior Nationals 2023-24
+               and numerous medals in other competitions. Our dedication and hard work have been recognized, and we
+               recently had the honor of visiting the Haryana Governors House to receive inspiration and accolades.
+               All the credit goes to our coach Dr. Takdir Kundu.
+              </p>
+              
+              <div className="flex justify-center mt-4">
+               <button className="button">About</button>
+               <button className="button">Achievements</button>
+               <button className="button">Gallery</button>
             </div>
+           </div>
+            
           </div>
-
-          <div className={`bg-white bg-opacity-20 rounded-lg p-8 flex items-center justify-center ${styles.contentSquare} ml-4`}>
-            <Image
-              src="/acro3.jpg"
-              alt="Image"
-              width={300}
-              height={200}
-            />
-          </div>
-        </div>
+          
+         </div>
+         <div className="image-container">
+             <img className="image" src="/acro1.jpg" alt="Image" />
+             <img className="image" src="/acro2.jpg" alt="Image" />
+             <img className="image" src="/acro3.jpg" alt="Image" />
+            </div>
 
         {/* Add more sections or content as needed */}
       </div>
